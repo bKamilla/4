80faf00f94e5f913c6274893e0215eb00efc2918
bKamilla <k.batrakova@g.nsu.ru>
2024-03-16 01:31:58 +0700

    2

--- 6.py
+++ 6.py
@@ -1,7 +1,7 @@
 first, second = input('Введите счет двух команд:').split(sep=':')
-if int(first) < int(second):
+if first < second:
     print(2)
-elif int(second) < int(first):
+elif second < first:
     print(1)
 else:
     print(0)
